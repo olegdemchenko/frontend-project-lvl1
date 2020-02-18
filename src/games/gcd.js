c6ac5e9f43c5a0426e004cad0993792adbe9a8f4
Lego <olegdemchenko1993@gmail.com>
2020-02-18 11:21:24 +0200

    refactoring: 6th step

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -6,20 +6,13 @@ const getGreatestDivisor = (number1, number2) => {
   if (number1 === 0 || number2 === 0) {
     return number1 === 0 ? number2 : number1;
   }
-  let greatestDivisor = 0;
-  let denom = 1;
-  while (denom <= Math.min(number1, number2)) {
-    if (number1 % denom === 0 && number2 % denom === 0) {
-      greatestDivisor = denom;
-    }
-    denom += 1;
-  }
-  return greatestDivisor;
+  return getGreatestDivisor(Math.min(number1, number2),
+    (Math.max(number1, number2) % Math.min(number1, number2)));
 };
 const genGameData = () => {
   const numb1 = randomNumbGen(0, 100);
   const numb2 = randomNumbGen(0, 100);
   const question = String().concat(numb1, ', ', numb2);
-  return { question, correctAnswer: `${getGreatestDivisor(numb1, numb2)}` };
+  return { question, correctAnswer: String(getGreatestDivisor(numb1, numb2)) };
 };
 export default () => runEngine(genGameData, description);
